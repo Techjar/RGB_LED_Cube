50b85911d6f962f44ddbe4fb1a25d7ea6f221421
Techjar <tecknojar@gmail.com>
2015-03-12 06:45:38 -0400

    Lots of work on TLC5940 code

--- include/main.h
+++ include/main.h
@@ -3,22 +3,28 @@
 //#define min(a,b) (a < b ? a : b)
 //#define max(a,b) (a > b ? a : b)
 
-#define BAM_BITS 4
-#define DATA_SIZE 192 * BAM_BITS
-#define MULTIPLEX_INTERVAL 46 // microseconds (change to 11 for 6-bit)
+#define DATA_SIZE 2304
+#define TLC5940_DATA_SIZE 288
 
-#define RX_BUFFER_SIZE 4096
+#define RX_BUFFER_SIZE 8192
 #define DMA_BUFFER_SIZE 192
+#define SPI_BUFFER_SIZE TLC5940_DATA_SIZE + 1
 
-//uint8_t spi_buffer[25]; // 25 8-bit shift registers
+#define TLC5940_GSCLK_COUNTS 4096
+#define TLC5940_GSCLK_FREQ 5000000
+#define TLC5940_BLANK_COUNT 50
+#define TIM_APB1_FREQ 84000000
+
+#define GPIO_LATCH_PIN GPIO_Pin_2
+#define GPIO_DCPRG_PIN GPIO_Pin_3
+#define GPIO_VPRG_PIN GPIO_Pin_4
+
 uint8_t dma_buffer[DMA_BUFFER_SIZE];
+uint8_t spi_buffer[SPI_BUFFER_SIZE];
 uint8_t rx_buffer[RX_BUFFER_SIZE];
 volatile int rx_buffer_head;
 volatile int rx_buffer_tail;
 
-void multiplex(void);
-inline void Latch_Data(void);
-void SPI_Transfer(uint8_t value);
 void Serial_Write(uint8_t value);
 void Serial_WriteBytes(uint8_t* buf, int length);
 //uint8_t Serial_Read();
--- src/main.c
+++ src/main.c
@@ -33,19 +33,11 @@
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
 #pragma GCC diagnostic ignored "-Wreturn-type"
 
-uint8_t bam_counts[BAM_BITS];
 uint8_t* front_buffer;
 uint8_t* back_buffer;
-uint8_t buffer_ready;
-
-int shift_out;
-int shift_out_start;
-int shift_out_end;
-int bit_mod;
-int color_mod;
-int level = 0; // keeps track of which level we are shifting data to
-int anodelevel = 0; // this increments through the anode levels
-int bam_bit, bam_counter = 0; // Bit Angle Modulation variables to keep track of things
+volatile uint8_t buffer_ready;
+volatile uint8_t spi_ready;
+volatile int level = 0; // this increments through the anode levels
 
 int min(int a, int b) {
 	return a < b ? a : b;
@@ -69,11 +61,14 @@ int main(void) {
 	NVIC_InitTypeDef NVIC_InitStruct;
 	DMA_InitTypeDef DMA_InitStruct;
 	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
+	TIM_OCInitTypeDef TIM_OCInitStruct;
+	TIM_TimeBaseStructInit(&TIM_TimeBaseInitStruct);
+    TIM_OCStructInit(&TIM_OCInitStruct);
 
 	//RCC_PCLK2Config(RCC_HCLK_Div16);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOB | RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOD, ENABLE);
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2 | RCC_APB1Periph_TIM2, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1 | RCC_AHB1Periph_DMA2, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2 | RCC_APB1Periph_TIM3 | RCC_APB1Periph_TIM4, ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
 
 	// LED pins
@@ -91,17 +86,8 @@ int main(void) {
 	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
 	GPIO_Init(GPIOD, &GPIO_InitStruct);
-
 	GPIO_PinAFConfig(GPIOD, GPIO_PinSource5, GPIO_AF_USART2);
 	GPIO_PinAFConfig(GPIOD, GPIO_PinSource6, GPIO_AF_USART2);
-
-	USART_InitStruct.USART_BaudRate = 2000000;
-	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
-	USART_InitStruct.USART_StopBits = USART_StopBits_1;
-	USART_InitStruct.USART_Parity = USART_Parity_No;
-	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-	USART_Init(USART2, &USART_InitStruct);
 
 	// SPI pins
 	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_7;
@@ -112,9 +98,26 @@ int main(void) {
 	GPIO_Init(GPIOA, &GPIO_InitStruct);
 	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_3;
 	GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource3, GPIO_AF_SPI1);
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource3, GPIO_AF_SPI1);
+
+	// Timer 3 & 4 pins
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_6;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
+	GPIO_Init(GPIOB, &GPIO_InitStruct);
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource4, GPIO_AF_TIM3);
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_TIM4);
+
+	USART_InitStruct.USART_BaudRate = 2000000;
+	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;
+	USART_InitStruct.USART_Parity = USART_Parity_No;
+	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART2, &USART_InitStruct);
 
 	SPI_I2S_DeInit(SPI1);
 	SPI_InitStruct.SPI_Mode = SPI_Mode_Master;
@@ -128,16 +131,17 @@ int main(void) {
 	SPI_InitStruct.SPI_CRCPolynomial = 0;
 	SPI_Init(SPI1, &SPI_InitStruct);
 
-	// Latch & blank pins
-	// Pin 2 = Latch, Pin 3 = Blank
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+	// LATCH, DCPRG, VPRG & 74HC595 BLANK pins
+	GPIO_InitStruct.GPIO_Pin = GPIO_LATCH_PIN | GPIO_DCPRG_PIN | GPIO_VPRG_PIN | GPIO_Pin_5;
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
 	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
 	GPIO_Init(GPIOA, &GPIO_InitStruct);
-	GPIOA->BSRRL = GPIO_Pin_3;
+	GPIOA->BSRRL = GPIO_Pin_5; // 74HC595 Output Enable
+	GPIOA->BSRRL = GPIO_DCPRG_PIN;
 
+	// UART DMA
 	DMA_InitStruct.DMA_Channel = DMA_Channel_4;
 	DMA_InitStruct.DMA_PeripheralBaseAddr = (uint32_t)&(USART2->DR);
 	DMA_InitStruct.DMA_Memory0BaseAddr = (uint32_t)&dma_buffer;
@@ -155,6 +159,24 @@ int main(void) {
 	DMA_InitStruct.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
 	DMA_Init(DMA1_Stream5, &DMA_InitStruct);
 
+	// SPI DMA
+	DMA_InitStruct.DMA_Channel = DMA_Channel_3;
+	DMA_InitStruct.DMA_PeripheralBaseAddr = (uint32_t)&(SPI1->DR);
+	DMA_InitStruct.DMA_Memory0BaseAddr = (uint32_t)&spi_buffer;
+	DMA_InitStruct.DMA_DIR = DMA_DIR_MemoryToPeripheral;
+	DMA_InitStruct.DMA_BufferSize = SPI_BUFFER_SIZE;
+	DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	DMA_InitStruct.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	DMA_InitStruct.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+	DMA_InitStruct.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+	DMA_InitStruct.DMA_Mode = DMA_Mode_Normal;
+	DMA_InitStruct.DMA_Priority = DMA_Priority_High;
+	DMA_InitStruct.DMA_FIFOMode = DMA_FIFOMode_Disable;
+	DMA_InitStruct.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
+	DMA_InitStruct.DMA_MemoryBurst = DMA_MemoryBurst_Single;
+	DMA_InitStruct.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
+	DMA_Init(DMA2_Stream5, &DMA_InitStruct);
+
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
 
 	USART_DMACmd(USART2, USART_DMAReq_Rx, ENABLE);
@@ -164,111 +186,96 @@ int main(void) {
 	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStruct);
 
+	SPI_I2S_DMACmd(SPI1, SPI_I2S_DMAReq_Tx, ENABLE);
+	/*DMA_ITConfig(DMA2_Stream5, DMA_IT_TC, ENABLE);
+	NVIC_InitStruct.NVIC_IRQChannel = DMA2_Stream5_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStruct);*/
+
 	USART_Cmd(USART2, ENABLE);
 	DMA_Cmd(DMA1_Stream5, ENABLE);
 	SPI_Cmd(SPI1, ENABLE);
 
-	TIM_TimeBaseInitStruct.TIM_Period = MULTIPLEX_INTERVAL - 1;
-	TIM_TimeBaseInitStruct.TIM_Prescaler = 84 - 1; // 1 Mhz
+	TIM_TimeBaseInitStruct.TIM_Period = 1;
+	TIM_TimeBaseInitStruct.TIM_Prescaler = ((TIM_APB1_FREQ / TLC5940_GSCLK_FREQ) / 4) - 1;
 	TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
 	TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStruct);
-	TIM_Cmd(TIM2, ENABLE);
+	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStruct);
+	TIM_OCInitStruct.TIM_OCMode = TIM_OCMode_Toggle;
+	TIM_OCInitStruct.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStruct.TIM_Pulse = 1;
+	TIM_OCInitStruct.TIM_OCPolarity = TIM_OCPolarity_High;
+	TIM_OC1Init(TIM3, &TIM_OCInitStruct);
+	TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
+	TIM_TimeBaseInitStruct.TIM_Period = TLC5940_GSCLK_COUNTS + TLC5940_BLANK_COUNT;
+	TIM_TimeBaseInitStruct.TIM_Prescaler = 0;
+	TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_CenterAligned1;
+	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseInitStruct);
+	TIM_OCInitStruct.TIM_OCMode = TIM_OCMode_PWM1;
+	TIM_OCInitStruct.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStruct.TIM_Pulse = TLC5940_BLANK_COUNT;
+	TIM_OCInitStruct.TIM_OCPolarity = TIM_OCPolarity_High;
+	TIM_OC1Init(TIM4, &TIM_OCInitStruct);
+	TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+	TIM_SelectOutputTrigger(TIM3, TIM_TRGOSource_Update);
+	TIM_SelectMasterSlaveMode(TIM3, TIM_MasterSlaveMode_Enable);
+	TIM_SelectInputTrigger(TIM4, TIM_TS_ITR2);
+	TIM_SelectSlaveMode(TIM4, TIM_SlaveMode_External1);
+	TIM_ITConfig(TIM4, TIM_IT_CC1, ENABLE);
 
 	front_buffer = (uint8_t*)malloc(DATA_SIZE);
 	back_buffer = (uint8_t*)malloc(DATA_SIZE);
 	memset(front_buffer, 0, DATA_SIZE);
 
-	for (int i = 0, count = 0; i < BAM_BITS; i++) {
-		count += 8 * pow(2, i);
-		bam_counts[i] = count;
+	{ // Fill the dot correction register and zero the anode control register
+		GPIOA->BSRRL = GPIO_VPRG_PIN;
+		delay(1);
+		int size = (TLC5940_DATA_SIZE / 2) + 1;
+		DMA2_Stream5->NDTR = size;
+		memset(spi_buffer, 0xFF, size - 1);
+		spi_buffer[size] = 0;
+		DMA_Cmd(DMA2_Stream5, ENABLE);
+		while (DMA_GetCmdStatus(DMA2_Stream5));
+		DMA2_Stream5->NDTR = SPI_BUFFER_SIZE;
+		delay(1);
+		GPIOA->BSRRL = GPIO_LATCH_PIN;
+		delay(1);
+		GPIOA->BSRRH = GPIO_LATCH_PIN;
+		delay(1);
+		GPIOA->BSRRH = GPIO_VPRG_PIN;
+		delay(1);
+		GPIOA->BSRRH = GPIO_Pin_5; // 74HC595 Output Enable
+		delay(1);
 	}
-	for (int i = 0; i < 25; i++) SPI_Transfer(0);
-	Latch_Data();
-	GPIOA->BSRRH = GPIO_Pin_3;
+
+	TIM_Cmd(TIM3, ENABLE);
+	TIM_Cmd(TIM4, ENABLE);
 
 	while (1) {
 		if (!buffer_ready && Serial_Available() >= DATA_SIZE) {
 			Serial_ReadBytes(back_buffer, DATA_SIZE);
 			buffer_ready = 1;
 		}
-		if (buffer_ready && bam_counter == 0) {
-			buffer_swap(&front_buffer, &back_buffer);
-			buffer_ready = 0;
-		}
-		if (TIM_GetFlagStatus(TIM2, TIM_FLAG_Update)) {
-			TIM_ClearFlag(TIM2, TIM_FLAG_Update);
-			multiplex();
+		if (spi_ready) {
+			if (buffer_ready && level == 0) {
+				buffer_swap(&front_buffer, &back_buffer);
+				buffer_ready = 0;
+			}
+			while (DMA_GetCmdStatus(DMA2_Stream5));
+			GPIOA->BSRRL = GPIO_LATCH_PIN;
+			memcpy(&spi_buffer[0], &front_buffer[TLC5940_DATA_SIZE * level], TLC5940_DATA_SIZE);
+			spi_buffer[TLC5940_DATA_SIZE] = 1 << level;
+			if (level++ == 8) level = 0;
+			GPIOA->BSRRH = GPIO_LATCH_PIN;
+			DMA_Cmd(DMA2_Stream5, ENABLE);
 		}
 	}
 
 	return 0;
-}
-
-void multiplex(void) {
-	// Data ordering in ledBits array:
-	// Each "sub-array" is 64 bytes, which is 512 bits, one for each LED.
-	// It's RGB, so there's 3 arrays of 64 bytes each.
-	// We have 4 sets of these, one for each BAM bit, since we're using 4 bit BAM.
-	// Each of these "sub-arrays" is then ordered with each layer using 8 bytes
-	// red0 green0 blue0
-	// red1 green1 blue1
-	// red2 green2 blue2
-	// red3 green3 blue3
-
-	// Turn off LEDs
-	//GPIOA->BSRRH = GPIO_Pin_2;
-	//SPI_Transfer(0);
-	//GPIOA->BSRRL = GPIO_Pin_2;
-	for (int i = 0; i < 25; i++) SPI_Transfer(0);
-	Latch_Data();
-	//GPIOA->BSRRH = GPIO_Pin_2;
-
-	bit_mod = 192 * bam_bit; // each 192 bytes in the array is one of the BAM bits for all the LEDs
-	for (color_mod = 0; color_mod < 192; color_mod += 64) {
-		shift_out_start = bit_mod + color_mod + level;
-		shift_out_end = shift_out_start + 8;
-		// Accidentally swapped the 2 cathode boards for each color, couldn't fix it in hardware so we'll just fix it here :)
-		for (shift_out = shift_out_end - 5; shift_out >= shift_out_start; shift_out--) {
-			SPI_Transfer(front_buffer[shift_out]);
-		}
-		for (shift_out = shift_out_end - 1; shift_out >= shift_out_start + 4; shift_out--) {
-			SPI_Transfer(front_buffer[shift_out]);
-		}
-	}
-	SPI_Transfer(1 << anodelevel);
-	Latch_Data();
-	// Turn off LEDs, latch data, turn on LEDs (not in use due to parasitic capacitance)
-	/*GPIOA->BSRRL = GPIO_Pin_3;
-	GPIOA->BSRRL = GPIO_Pin_2;
-	GPIOA->BSRRH = GPIO_Pin_2;
-	GPIOA->BSRRH = GPIO_Pin_3;*/
-
-	bam_counter++;
-	if (bam_counter == bam_counts[bam_bit]) {
-		bam_bit++;
-		if (bam_bit == BAM_BITS) {
-			bam_counter = 0;
-			bam_bit = 0;
-		}
-	}
-
-	anodelevel++;
-	level += 8;
-	if (anodelevel == 8) anodelevel = 0;
-	if (level == 64) level = 0;
-}
-
-inline void Latch_Data(void) {
-	for (int i = 0; i < 70; i++) asm("nop");
-	GPIOA->BSRRL = GPIO_Pin_2;
-	for (int i = 0; i < 70; i++) asm("nop");
-	GPIOA->BSRRH = GPIO_Pin_2;
-}
-
-void SPI_Transfer(uint8_t value) {
-	SPI_I2S_SendData(SPI1, value);
-	while (!SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE));
 }
 
 void Serial_Write(uint8_t value) {
